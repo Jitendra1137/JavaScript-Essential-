6fe167b3f45b5e7db9056d051cf28859b494d660
Jitendra Kumar Jat <jitendrakumarjat81@gmail.com>
2024-04-06 22:50:02 +0530

    save

---
+++ 1st.js
@@ -0,0 +1,21 @@
+// template literal 
+
+const age =20;
+const fullName = "jitendra jat";
+
+// output: Hello Mr jitendra jat your age is : 22
+
+
+console.log("hello Mr " +fullName+ " your age is: " +age);
+
+console.log(`hello mr ${fullName} your age is: ${age}`);
+
+
+
+// print sum of two numbers
+
+
+const a = 10;
+const b = 10;
+
+console.log(` sum of  ${a} and  b is ${b}  ${a+b}`);
